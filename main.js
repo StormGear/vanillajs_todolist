4621f1d39b6c8859fdd20358d1ff6d6ee1f170c9
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-19 20:15:07 +0000

    add forms and form validation for login and signup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import date from 'date-and-time';
 
 
@@ -13,7 +14,6 @@ const firebaseConfig = {
 };
 
 let textFromInputField;
-let idCounter = 0;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -21,12 +21,96 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
+// Initialize Authentication
+const auth = getAuth();
+
+function createUser(email, password) {
+    if (validateEmail(signupEmail.value) && validatePassword(signupPassword.value)) {
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    console.log(`User created: User ${userCredential.user}`);
+  })
+  .catch((error) => {
+    console.log(`Error creating user with code ${error.code} and message ${error.message}`);
+  });
+} else {
+    console.log('Enter valid email or password.')
+}
+}
+
 
 listenToTodosChanges()
 
 // Add click event listened to get text from the input field
 document.getElementById("add-task-button").addEventListener("click", addTodoToFirestore);
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
+const signupEmail = document.getElementById("signup-email");
+const signupPassword = document.getElementById("signup-password");
 
+// Add blur event listener to validate email and password
+loginEmail.addEventListener('blur', (e) => validateLoginEmailAndPassword(e));
+loginPassword.addEventListener('blur', (e) => validateLoginEmailAndPassword(e));
+// Add click event listener to validate email and password
+document.getElementById("login-button").addEventListener("blur", (e) => validateLoginEmailAndPassword(e));
+
+// Add click event listener to validate email and password
+document.getElementById("signup-button").addEventListener("click", (e) => { 
+    validateSignupEmailAndPassword(e);
+    createUser(signupEmail.value, signupPassword.value);
+});
+// Add blur event listener to validate email and password
+signupEmail.addEventListener('blur', (e) => validateSignupEmailAndPassword(e));
+signupPassword.addEventListener('blur', (e) => validateSignupEmailAndPassword(e));
+
+function validateSignupEmailAndPassword(event) {
+    event.preventDefault();
+
+    if (validateEmail(signupEmail.value)) {
+        console.log('Email is valid');
+        signupEmail.nextElementSibling.style.display = "block";
+        signupEmail.nextElementSibling.nextElementSibling.style.display = "none";
+    } else {
+        console.log('Email is invalid');
+        signupEmail.nextElementSibling.style.display = "none";
+        signupEmail.nextElementSibling.nextElementSibling.style.display = "block";
+    }
+    if (validatePassword(signupPassword.value)) {
+        console.log('Password is valid');
+        signupPassword.nextElementSibling.style.display = "block";
+        signupPassword.nextElementSibling.nextElementSibling.style.display = "none";
+    } else {
+        console.log('Password is invalid');
+        signupPassword.nextElementSibling.style.display = "none";
+        signupPassword.nextElementSibling.nextElementSibling.style.display = "block";
+    }
+
+}
+
+function validateLoginEmailAndPassword(event) {
+    event.preventDefault();
+    // const email = document.getElementById("login-email");
+    // const password = document.getElementById("login-password");
+    if (validateEmail(loginEmail.value)) {
+        console.log('Email is valid');
+        loginEmail.nextElementSibling.style.display = "block";
+        loginEmail.nextElementSibling.nextElementSibling.style.display = "none";
+    } else {
+        console.log('Email is invalid');
+        loginEmail.nextElementSibling.style.display = "none";
+        loginEmail.nextElementSibling.nextElementSibling.style.display = "block";
+    }
+    if (validatePassword(loginPassword.value)) {
+        console.log('Password is valid');
+        loginPassword.nextElementSibling.style.display = "block";
+        loginPassword.nextElementSibling.nextElementSibling.style.display = "none";
+    } else {
+        console.log('Password is invalid');
+        loginPassword.nextElementSibling.style.display = "none";
+        loginPassword.nextElementSibling.nextElementSibling.style.display = "block";
+    }
+}
 
 
 
@@ -64,6 +148,17 @@ function deleteTodo(e) {
     deleteDoc(doc(db, "todos", e.target.parentElement.parentElement.parentElement.id)).then(() => console.log('Document deleted')).catch(e => console.error('Error deleting document', e));
 }
 
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+
+
+function validatePassword(password) {
+    return password.length > 6;
+}
+
 function editTodoModal(e) { 
     const element = e.target.parentElement.parentElement.parentElement;
     console.log(element.id);
@@ -85,12 +180,6 @@ function editTodoModal(e) {
         });
     });
 }
-
-function saveChanges(e, id) {
-   
-
-}
-
 
 
 
