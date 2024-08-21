0b31327ab885db93495f1e01a4a398ad51fd57d4
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-21 20:37:21 +0000

    conform folder struct

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // This is the root directory of the project
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+});
