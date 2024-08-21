0c39a98df63fc64f53323630f2bdf781ac65acbb
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-08-21 20:52:47 +0000

    added a src folder

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: '.', // This is the root directory of the project
-  build: {
-    outDir: 'dist', // Default output directory
-  },
-});
